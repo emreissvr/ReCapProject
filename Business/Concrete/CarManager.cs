0978cf6490f924999432755013819668b420b700
emreissvr <emreiissever@gmail.com>
2021-05-14 12:24:05 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+
+        ICarDal _carDal;
+
+        public CarManager(ICarDal ıcarDal)
+        {
+            _carDal = ıcarDal;
+        }
+
+        
+
+        // business ın bildiği tek şey ICarDal'dır.
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
